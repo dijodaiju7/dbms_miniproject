0acc38348022b411e62e78463692073616fea288
Dijo Daiju <60286610+dijodaiju7@users.noreply.github.com>
2022-03-03 18:37:21 +0530

    updated

--- deposit.php
+++ deposit.php
@@ -6,7 +6,7 @@ include 'dbconnection.php';
 ?>
 <!DOCTYPE html>
 <head>
-    <title>Fund Transfer</title>
+    <title>Deposit</title>
 </head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
